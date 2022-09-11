907a4f8e3ce6a63bc9ef64f6c6aad687c32a03e2
Unnati Makrariya <unnatimakrariya@gmail.com>
2022-09-11 19:52:08 +0530

    done final

--- app.js
+++ app.js
@@ -6,30 +6,29 @@ var outputBox = document.querySelector("#output-box");
 
 submitBtn.addEventListener('click', submitHandler);
 
-function submitHandler(){
+function submitHandler() {
     var ip = Number(initialPrice.value);
     var qty = Number(stocksQuantity.value);
     var cur = Number(currentPrice.value);
     calculateProfitAndLoss(ip, qty, cur);
 }
-function calculateProfitAndLoss(initial, quantity, current){
-    if(initial> current){
-var loss = (initial - current)*quantity;
-var lossPercentage = (loss / initial) * 100;
-   showOutput('The loss is:'+loss+'and the percent is'+lossPercentage+'%');
-    }
-    else if(current > initial){
-var profit = (current - initial) * quantity;
-var profitPercentage = (profit / initial) * 100;
-showOutput('The profit is:'+profit+'and the percent is'+ profitPercentage+'%');
-    }
-    else{
-showOutput("No Pain No Gain ; and No Gain No Pain👌");
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        showOutput('The loss is:' + loss + 'and the percent is' + lossPercentage + '%');
+    } else if (current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+        showOutput('The profit is:' + profit + 'and the percent is' + profitPercentage + '%');
+    } else {
+        showOutput("No Pain No Gain and No Gain No Pain");
     }
 }
-function showOutput(message){
-    
-            outputBox.innerHTML = message;
-    
-    }
 
+function showOutput(message) {
+
+    outputBox.innerHTML = message;
+
+}
